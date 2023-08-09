cf93d8ed6b025f7f891b7240a7b03b16dd1a6500
WeiChiaKung <101169427+WeiChiaKung@users.noreply.github.com>
2023-08-09 12:33:02 +0800

    Update task4.v

--- LAB03/task4/task4.v
+++ LAB03/task4/task4.v
@@ -1,1 +1,11 @@
+`timescale 1ns / 1ps
+module task4(
+    input CLK,
+    input rst_n,
+    output [7:0]s
+    );
+    wire CLK_temp;
+    freq_div U0(.CLK(CLK), .rst_n(rst_n), .CLK_out(CLK_temp));
+    ring_counter(.CLK(CLK_temp), .rst_n(rst_n), .s(s));
+endmodule
 
