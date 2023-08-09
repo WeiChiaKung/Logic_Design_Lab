72ad40c5c77cc64719967d97d0a7bc1473cd6022
WeiChiaKung <101169427+WeiChiaKung@users.noreply.github.com>
2023-08-09 13:51:48 +0800

    Update task1.v

--- LAB04/task1/task1.v
+++ LAB04/task1/task1.v
@@ -1,1 +1,20 @@
+`timescale 1ns / 1ps
+module task1(
+    input CLK,
+    input rst_n,
+    output reg [3:0]b
+    );
+    wire CLK_temp;
+    reg [3:0]b_temp;
+    freq_div U0(.CLK(CLK), .rst_n(rst_n), .CLK_out(CLK_temp));
+    always@(*)
+        b_temp = b-1;
+    always@(posedge CLK_temp or negedge rst_n)
+    begin
+        if(~rst_n)
+            b <= 4'b1111;
+        else
+            b <= b_temp;
+    end
+endmodule
 
