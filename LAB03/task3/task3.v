bdffa21fa0964c60e253a63e4fc106f1abb61829
WeiChiaKung <101169427+WeiChiaKung@users.noreply.github.com>
2023-08-09 12:31:25 +0800

    Update task3.v

--- LAB03/task3/task3.v
+++ LAB03/task3/task3.v
@@ -1,1 +1,11 @@
+`timescale 1ns / 1ps
+module task3(
+    input CLK,
+    input rst_n,
+    output [3:0]q
+    );
+    wire CLK_temp;
+    freq_div U0(.CLK(CLK), .rst_n(rst_n), .CLK_out(CLK_temp));
+    up_counter_4bit U1(.CLK(CLK_temp), .rst_n(rst_n), .count(q));
+endmodule
 
